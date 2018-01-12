4f560b3b4c7205a6ccc0798a75fef819a1e36863
sathyar27 <tosathyar@gmail.com>
2018-01-12 14:56:17 +0530

    Added Comments on HTML and added js file separately

---
+++ asset/js/app.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+    if($(window).width() < 992){
+        $('.callouts ul').addClass('owl-carousel owl-theme');
+    }
+    else {
+        $('.callouts ul').trigger('destroy.owl.carousel');
+        $('.callouts ul').removeClass('owl-carousel owl-theme');
+    }
+    $('.callouts .owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+    if($(window).width() >= 992) {
+        $('.lonelyGingerContent figure').appendTo('.gingerImage');
+        $('.fightingPowerContent figure').appendTo('.fightingImage');
+    }
+    else {
+        $( '.lonelyGingerContent figure' ).next( ".lonelyGingerContent h1" );
+        $( '.fightingPowerContent figure' ).next( ".fightingPowerContent h1" );
+    }
+
+    $(window).resize(function() {
+        if($(window).width() < 992){
+            $('.callouts ul').addClass('owl-carousel owl-theme');
+        }
+        else {
+            $('.callouts ul').trigger('destroy.owl.carousel');
+            $('.callouts ul').removeClass('owl-carousel owl-theme');
+        }
+        $('.callouts .owl-carousel').owlCarousel({
+            loop:true,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                }
+            }
+        });
+        if($(window).width() >= 992) {
+            $('.lonelyGingerContent figure').appendTo('.gingerImage');
+            $('.fightingPowerContent figure').appendTo('.fightingImage');
+        }
+        else {
+            $('.lonelyGingerContent figure').next(".lonelyGingerContent h1");
+            $('.fightingPowerContent figure').next(".fightingPowerContent h1");
+        }
+    });
+});
